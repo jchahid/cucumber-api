ef3573806202318629549aff50fd60000072d5f0
jchahid <jalal.chahid.86@gmail.com>
2024-07-17 23:44:03 +0200

    first commit

---
+++ src/main/java/fr/capgemini/cucumber_api/CucumberApiApplication.java
@@ -0,0 +1,13 @@
+package fr.capgemini.cucumber_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CucumberApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CucumberApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/fr/capgemini/cucumber_api/CucumberApiApplicationTests.java
@@ -0,0 +1,13 @@
+package fr.capgemini.cucumber_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CucumberApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
